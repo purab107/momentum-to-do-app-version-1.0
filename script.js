a8facc1ad8b82efa216270ed3593fb02b79b2713
Purab Sahare <purabsahare@gmail.com>
2026-06-16 00:43:41 +0530

    create-node-added

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-let task = document.querySelector('#user-input')
-let inputButton = document.querySelector('#input-button')
-let taskListDiv = document.querySelector('#task-list')
+const form = document.querySelector('#user-input-field')
+const taskList = document.querySelector('#task-list-tree')
 
-let taskListArray = [];
-// task.addEventListener('input', (event) => {
-//     console.log('Live text:', event.target.value);
-// });
+let taskArrayList = []
 
-task.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        console.log('Submitted text:', event.target.value);
-        taskListArray.push(event.target.value);
-        printArray()
-    }
+
+// form event listener -> collects user input
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    const taskInput = document.getElementById('user-input').value;
+    createTaskNode(taskInput)
+    console.log(taskInput)
 })
 
-function printArray() {
-    taskListArray.forEach(element => {
-        console.log(`array element : ${element}`)
-    });
+// creates a task node -> check box | task name | delete button
+function createTaskNode(task) {
+    const li = document.createElement('li')
+
+    // creating task name
+    const taskName = document.createElement('span')
+    taskName.textContent = task;
+    //li.textContent = task;
+    li.appendChild(taskName)
+    
+
+
+    // creating delete button
+    const dltButton = document.createElement('button')
+    dltButton.textContent = "Delete";
+    li.appendChild(dltButton)
+
+
+    // appending the nodes to the main tree
+    taskList.appendChild(li);
 }
+
+function renderTaskList() {}
